35062da37e95605dac046473525f088d3ec6c2f5
lmh91 <lhauert@mpp.mpg.de>
2017-06-26 11:25:16 +0200

    implemented encoding/decoding functions

--- src/TMCM3110.jl
+++ src/TMCM3110.jl
@@ -1,5 +1,27 @@
 module TMCM3110
 
-# package code goes here
+function encode_command(m_address, n_command, n_type, n_motor, value)
+  m_address = UInt8( m_address % (1<<8) )
+  n_command = UInt8( n_command % (1<<8) )
+  n_type    = UInt8( n_type % (1<<8)    )
+  n_motor   = UInt8( n_motor % (1<<8)   )
+  value     = [ UInt8( (value >> i*8) % (1<<8) ) for i in range(3,-1,4)]
 
+  checksum = UInt8( (m_address + n_command + n_type + n_motor + sum(value)) % (1<<8) )
+
+  tmcl_bytes = [m_address, n_command, n_type, n_motor, value..., checksum]
+  # tmcl_cmd = sum(Int128(b) << (9-i)*8 for (i,b) in enumerate(tmcl_bytes))
+
+
+  command = String(transcode(UInt8, tmcl_bytes));
+  return command
+end
+
+function decode_reply(reply)
+  reply = Vector{UInt8}(reply)
+  reply = Vector{Int}(reply)
+  return reply
+end
+
+# test
 end # module
